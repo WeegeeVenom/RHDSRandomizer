042ead845b27e4444a4b39cd7f8436fe51dbd0f4
WeegeeVenom <weegee100@gmail.com>
2023-04-12 16:46:11 +0900

    add randomizer.py, setOfOverlays.txt

--- randomizer.py
+++ randomizer.py
@@ -1,13 +1,8 @@
 import random
+from datetime import datetime
 GAME_OVERLAYS_ADDR = 0x10D50
 GAMES_BINARYLIST = [0,0,0,1,1,1,1,1,1,0,0,1,1,0,0,0,0,0,0,1,0,0,0,1,1,1,0,0,1,1,1,1,1,1,0,1,1,1,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,1,0,0,0,0,0,0,0,0,0,0,1,0]
-#TODO: GET THE HEX TABLE IN A LIST
-#RANDOMIZE LIST
-#DONE - UPDATE THE FILES PROPERLY
-#TODO: FIGURE OUT HOW TO SKIP AND UPDATE THE arm09.bin FILE
-	# Use a dict with matching values of the first bytes
-	# And use random.Random(seed).shuffle(keys)
-	# Offset the OVERLAYS_ADDR by the key - base key
+
 
 def fileExtractor(binaryList, inFile, address, bytesCount): 
 	outDict = {}
@@ -29,12 +24,10 @@ def fileEditor(inDict, filename, startAddr, keys, bytesCount, offSet):
 			editF.write(ele)
 			i += 1
 
-seed = 387179039
-#seed = random.Random(555555555)
+
+seed = datetime.now().timestamp()
 outDict = fileExtractor(GAMES_BINARYLIST, 'arm9OG.bin', GAME_OVERLAYS_ADDR, 8)
 keys = list(outDict.keys())
-print(keys)
-random.Random(seed).shuffle(keys)
-print(outDict)
-fileEditor(outDict, 'arm9OGpywritetest.bin', GAME_OVERLAYS_ADDR, keys, 8, 60)
-#Starts at 60, so do num - 60 * 3
+random.seed(seed)
+random.Random().shuffle(keys)
+fileEditor(outDict, 'arm9.bin', GAME_OVERLAYS_ADDR, keys, 8, 60)
