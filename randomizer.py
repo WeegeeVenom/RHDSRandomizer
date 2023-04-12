93fc694e8383dc7a7fb3327a234fdf4938a476bc
WeegeeVenom <weegee100@gmail.com>
2023-04-12 23:14:30 +0900

    error handling + fixed GAMES_BINARYLIST

--- randomizer.py
+++ randomizer.py
@@ -1,7 +1,19 @@
 import random
 from datetime import datetime
+from traceback import print_exc
+from sys import exc_info
 GAME_OVERLAYS_ADDR = 0x10D50
-GAMES_BINARYLIST = [0,0,0,1,1,1,1,1,1,0,0,1,1,0,0,0,0,0,0,1,0,0,0,1,1,1,0,0,1,1,1,1,1,1,0,1,1,1,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,1,0,0,0,0,0,0,0,0,0,0,1,0]
+#GAMES_BINARYLIST is offset by 60 (first )
+GAMES_BINARYLIST = [0,0,0,1,1,1,1,1,1,0,
+					0,0,1,1,0,0,0,0,0,1,
+					0,0,0,1,1,1,0,0,1,1,
+					1,1,1,1,0,1,1,1,0,0,
+					0,1,1,1,1,1,1,1,1,1,
+					1,1,1,1,1,0,0,0,0,0,
+					0,0,0,0,0,0,1,0,0,0,
+					0,0,1,1,1,1,1,1,1,1,
+					1,1,1,1,0,0,0,1,0,0,
+					0,0,0,0,0,0,0,0,1,0]
 
 
 def fileExtractor(binaryList, inFile, address, bytesCount): 
@@ -24,10 +36,19 @@ def fileEditor(inDict, filename, startAddr, keys, bytesCount, offSet):
 			editF.write(ele)
 			i += 1
 
-
-seed = datetime.now().timestamp()
-outDict = fileExtractor(GAMES_BINARYLIST, 'arm9.bin', GAME_OVERLAYS_ADDR, 8)
-keys = list(outDict.keys())
-random.seed(seed)
-random.Random().shuffle(keys)
-fileEditor(outDict, 'arm9.bin', GAME_OVERLAYS_ADDR, keys, 8, 60)
+if __name__ == '__main__':
+	try:
+		seed = datetime.now().timestamp() 
+		outDict = fileExtractor(GAMES_BINARYLIST, 'arm9.bin', GAME_OVERLAYS_ADDR, 8) #dictionary for game overlays
+		print(outDict)
+		with open('test.bin', 'wb') as f:
+			for _, ele in outDict.items():
+				f.write(ele)
+		keys = list(outDict.keys())
+		random.seed(seed)
+		random.Random().shuffle(keys)
+		fileEditor(outDict, 'test2.bin', GAME_OVERLAYS_ADDR, keys, 8, 60)
+	except Exception:
+		print_exc()
+		print('ERROR:', exc_info()[1])
+		input('Press Enter to close this program. ')
