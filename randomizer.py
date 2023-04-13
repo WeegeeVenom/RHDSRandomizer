e53be9991c7472f59fef84b9e878e5a9183fba94
WeegeeVenom <weegee100@gmail.com>
2023-04-14 08:50:37 +0900

    ExceptionHandling + full filepath implement

--- randomizer.py
+++ randomizer.py
@@ -2,6 +2,7 @@ import random
 from datetime import datetime
 from traceback import print_exc
 from sys import exc_info
+from os import path
 GAME_OVERLAYS_ADDR = 0x10D50 #for arm9.bin
 DESCRIPTIONS_ADDR = 0x61C4 #for overlay9_18.bin
 #GAMES_BINARYLIST is offset by 60 (first is 60 or 3c)
@@ -16,26 +17,36 @@ GAMES_BINARYLIST = [0,0,0,1,1,1,1,1,1,0,
 					1,1,1,1,0,0,0,1,0,0,
 					0,0,0,0,0,0,0,0,1,0]
 
-
+filepath = __file__.rsplit('\\', 1)[0]
 #Generates dict of all of the overlays to be swapped
-def gameOverlayExtractor(binaryList, inFile, address, bytesCount): 
+def gameOverlayExtractor(binaryList, filename, address, bytesCount): 
 	outDict = {}
-	with open(inFile, 'rb') as inF:
-		inF.seek(address)
-		for i in binaryList:
-			bytesData = inF.read(bytesCount)
-			if i:
-				outDict[int.from_bytes(bytesData[0:2], byteorder = 'little')] = bytesData
-	return outDict
+	try:
+		with open(path.join(filepath, filename), 'rb') as inF:
+			inF.seek(address)
+			for i in binaryList:
+				bytesData = inF.read(bytesCount)
+				if i:
+					outDict[int.from_bytes(bytesData[0:2], byteorder = 'little')] = bytesData
+		return outDict
+	except Exception:
+		print_exc()
+		print('ERROR:', exc_info()[1])
+		input('Press Enter to close this program. ')
 
 #uses the shuffled dictionary to write back into the arm9.bin file
 def fileEditor(inDict, filename, startAddr, keys, bytesCount, offSet):
-	with open(filename, 'r+b') as editF:
-		i = 0
-		for _, ele in inDict.items():
-			editF.seek(startAddr + ((keys[i] - offSet) * bytesCount))
-			editF.write(ele)
-			i += 1
+	try:
+		with open(path.join(filepath, filename), 'r+b') as editF:
+			i = 0
+			for _, ele in inDict.items():
+				editF.seek(startAddr + ((keys[i] - offSet) * bytesCount))
+				editF.write(ele)
+				i += 1
+	except Exception:
+		print_exc()
+		print('ERROR:', exc_info()[1])
+		input('Press Enter to close this program. ')
 
 if __name__ == '__main__':
 	try:
