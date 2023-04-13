9afdb11ba439d8c807665771c414c9c98e3eed06
WeegeeVenom <weegee100@gmail.com>
2023-04-14 08:57:30 +0900

    FileNotFoundError Handling

--- randomizer.py
+++ randomizer.py
@@ -2,7 +2,7 @@ import random
 from datetime import datetime
 from traceback import print_exc
 from sys import exc_info
-from os import path
+from os import path, getcwd
 GAME_OVERLAYS_ADDR = 0x10D50 #for arm9.bin
 DESCRIPTIONS_ADDR = 0x61C4 #for overlay9_18.bin
 #GAMES_BINARYLIST is offset by 60 (first is 60 or 3c)
@@ -29,11 +29,18 @@ def gameOverlayExtractor(binaryList, filename, address, bytesCount):
 				if i:
 					outDict[int.from_bytes(bytesData[0:2], byteorder = 'little')] = bytesData
 		return outDict
+	except FileNotFoundError:
+		print_exc()
+		print('ERROR:', exc_info()[1])
+		print('Current working directory: ', getcwd())
+		input('Press Enter to close this program. ')
+		exit()
 	except Exception:
 		print_exc()
 		print('ERROR:', exc_info()[1])
 		input('Press Enter to close this program. ')
 		exit()
+
 
 #uses the shuffled dictionary to write back into the arm9.bin file
 def fileEditor(inDict, filename, startAddr, keys, bytesCount, offSet):
@@ -44,6 +51,12 @@ def fileEditor(inDict, filename, startAddr, keys, bytesCount, offSet):
 				editF.seek(startAddr + ((keys[i] - offSet) * bytesCount))
 				editF.write(ele)
 				i += 1
+	except FileNotFoundError:
+		print_exc()
+		print('ERROR:', exc_info()[1])
+		print('Current working directory: ', getcwd())
+		input('Press Enter to close this program. ')
+		exit()
 	except Exception:
 		print_exc()
 		print('ERROR:', exc_info()[1])
